25dc68178c586e8ab4438f1e72f4513f25e40df5
Ardrass2 <118733164+Ardrass2@users.noreply.github.com>
2024-02-24 20:58:18 +0300

    Add size and count of elements var. add new constructor. Rename function of removing

--- Array.h
+++ Array.h
@@ -4,10 +4,13 @@
 
 class Arry{
 	unsigned char* m_bytes;
+	size_t size = 1;
+	size_t countElement = 0;
 public:
+   	Arry();
 	Arry(size_t size);
 	void addElement(unsigned char elem);
-	void removeEleent(size_t pos);
+	void removeElement(size_t pos);
 	unsigned char getElement(size_t pos);
 	void setElement(size_t pos, unsigned char value);
 	void resize(size_t new_size);
