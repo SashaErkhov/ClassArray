68ef3c383f24105ac1865094adf4c998c6cc8d96
Ardrass2 <118733164+Ardrass2@users.noreply.github.com>
2024-02-27 10:44:10 +0300

    Update Array.h

--- Array.h
+++ Array.h
@@ -2,16 +2,43 @@
 #ifndef ARRAY_OUR_WORK
 #define ARRAY_OUR_WORK
 
-class Arry{
-	unsigned char* m_bytes;
+template <typename T> class Arry{
+	T* m_bytes;
 	size_t size = 1;
 	size_t countElement = 0;
 public:
-	Arry(size_t size=1);
-	void addElement(unsigned char elem);
-	void removeElement(size_t pos);
-	unsigned char getElement(size_t pos);
-	void setElement(size_t pos, unsigned char value);
+	Arry(size_t size) {
+	/*standart constructor with size*/
+	    m_bytes = new T[size];
+	}
+	void addElement(T elem) {
+	/*This function add new element. 
+ 	Its check size and if out size <
+  	than count of element do resize 
+	for size+1*/
+	    if (countElement + 1 > size) {
+	        resize(size + 1);
+	    }
+	    m_bytes[countElement] = elem;
+	    countElement++;
+	}
+	void removeElement(size_t pos) {
+	    /*This function remove eleemnt from pos
+		and move for 1 point other element better(for pos)
+	 	than this.*/
+	    if (pos >= size) {
+	        return;
+	    }
+	    
+	    for (size_t i = pos; i < size - 1; ++i) {
+	        m_bytes[i] = m_bytes[i + 1];
+	    }
+	    
+	    countElement--;
+	}
+
+	T getElement(size_t pos);
+	void setElement(size_t pos, T value);
 	void resize(size_t new_size);
 	~Arry();
 };
