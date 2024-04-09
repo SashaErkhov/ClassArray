83ed5939c492a776b2de8257dae64367cecd4637
77maxikov <77maxikov@mail.ru>
2024-04-09 11:26:58 +0300

    Прототип ассоциативного массива

---
+++ Assoc.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Arry.h"
+
+template < class Key, class Data> class Assoc{ 
+	// Key - то , по чему находим, 
+	// Data - то, что находим
+	struct node {
+		Key key;
+		Data data;
+	};
+	Arry<node> m_storage;
+public:
+	Assoc() {}		
+	addPair(Key k, Data d); // Добавляем соответствие
+
+		Data findByKey(Key k) { // Здесь генерируется исключение!!! (Возможно)
+
+			Arry<node>::iterator res = binarySearch(k, m_storage.begin(), m_storage.end());
+			if (res) { // Нашли
+			}
+			else { //Не нашли
+			}
+		}
+
+};
