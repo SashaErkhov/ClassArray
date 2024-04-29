83a981a732a4e77688ac3b452eddd1a00cf293ed
Ibragim Oskanov <Oskanov-ibragim.06@mail.ru>
2024-04-29 12:07:31 +0300

    refactor Arry.h

--- Arry.h
+++ Arry.h
@@ -4,74 +4,74 @@
 #include <iostream>
 #include <stdexcept>
 
-template <typename T> class Arry{
+template <typename T> class Arry {
 	T* m_bytes;
 	size_t size;
 public:
 	Arry(size_t size = 0) : size(size) {
-	    m_bytes = new T[size];
+		m_bytes = new T[size];
 	}
-	size_t getSize() const{ return size; }
-	void addElement( const T& elem) {
-	    T* new_bytes = new T[++size];
-        for (size_t i = 0; i < (size-1); ++i) {
-            new_bytes[i] = m_bytes[i];
-        }
-        new_bytes[size-1] = elem;
-        if(size!=1) delete[] m_bytes;
-        m_bytes = new_bytes;
+	size_t getSize() const { return size; }
+	void addElement(const T& elem) {
+		T* new_bytes = new T[++size];
+		for (size_t i = 0; i < (size - 1); ++i) {
+			new_bytes[i] = m_bytes[i];
+		}
+		new_bytes[size - 1] = elem;
+		if (size != 1) delete[] m_bytes;
+		m_bytes = new_bytes;
 	}
 	void removeElement(size_t pos) {
-	    if (pos >= size) {
-	        return;
-	    }
+		if (pos >= size) {
+			return;
+		}
 
-	    for (size_t i = pos; i < size - 1; ++i) {
-	        m_bytes[i] = m_bytes[i + 1];
-	    }
-        --size;
+		for (size_t i = pos; i < size - 1; ++i) {
+			m_bytes[i] = m_bytes[i + 1];
+		}
+		--size;
 	}
 
 	T getElement(size_t pos)
 	{
-		/*You can get element from array */
 		return m_bytes[pos];
 	}
-	void setElement(size_t pos, T value){
-		if (pos>size-1){
-			std::cout<<"position is out of range"<<std::endl;
+	void setElement(size_t pos, T value) {
+		if (pos > size - 1) {
+			std::cout << "position is out of range" << std::endl;
 		}
-		else{
-		m_bytes[pos]=value;
+		else {
+			m_bytes[pos] = value;
 		}
 	}
-	void resize(size_t new_size){
-		/*this resize function do new_size 
-    	for array*/
-		T* new_m_bytes=new T[new_size];
+	void resize(size_t new_size) {
+		/*this resize function do new_size
+		for array*/
+		T* new_m_bytes = new T[new_size];
 		size_t old_size = size;
-		if (old_size>=new_size){
-			for(size_t i=0; i<new_size; ++i){
-				new_m_bytes[i]=m_bytes[i];
+		if (old_size >= new_size) {
+			for (size_t i = 0; i < new_size; ++i) {
+				new_m_bytes[i] = m_bytes[i];
 			}
-		} else {
-			for(size_t i=0; i<old_size; ++i){
-				new_m_bytes[i]=m_bytes[i];
+		}
+		else {
+			for (size_t i = 0; i < old_size; ++i) {
+				new_m_bytes[i] = m_bytes[i];
 			}
-			for(size_t i=old_size; i < new_size; ++i){
-				new_m_bytes[i]=0;
+			for (size_t i = old_size; i < new_size; ++i) {
+				new_m_bytes[i] = 0;
 			}
 		}
 		delete[] m_bytes;
-		m_bytes=new_m_bytes;
+		m_bytes = new_m_bytes;
 		size = new_size;
 	}
 	~Arry()
 	{
 		delete[] m_bytes;
 	}
-	const T& operator[](size_t pos)const {return m_bytes[pos];}
-	T& operator[](size_t pos) {return m_bytes[pos];}
+	const T& operator[](size_t pos)const { return m_bytes[pos]; }
+	T& operator[](size_t pos) { return m_bytes[pos]; }
 
 	class iterator {
 		T* current;
@@ -116,24 +116,23 @@ public:
 			return *current < other;
 		}
 
-		friend class Arry; 
+		friend class Arry;
 	};
 
-	iterator begin(){
-	 	return iterator(m_bytes);
+	iterator begin() {
+		return iterator(m_bytes);
 	}
 
 	iterator end() {
 		return iterator(m_bytes + size);
 	}
 
-    void insert(const iterator& pos, const T& value);
+	void insert(const iterator& pos, const T& value);
 };
 
 template<typename T>
 void Arry<T>::insert(const typename Arry<T>::iterator& pos, const T& value)
 {
-	//Добавление элемента по итератору
 	T* new_bytes = new T[++size];
 	size_t i = 0;
 	while (((m_bytes + i) != (pos.current))and (i<(size-1)))
